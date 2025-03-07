ceac1978f60007975b33ae16543a725b8e4a49f5
JamilaFatima34 <jamilafatima7896@gmail.com>
2025-03-07 11:22:11 +0500

    Added Substraction Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,2 +1,9 @@
 Console.WriteLine("Jamila Fatima");
 Console.WriteLine("24-St-042");
+
+Code for Substraction:
+
+int num1 = 23;
+int num2 = 13;
+int difference = num1 + num2;
+Console.WriteLine("The difference between " + num1 + "and " + num2 + " is: "+ difference);
