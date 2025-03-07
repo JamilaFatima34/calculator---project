69d4cfb33c58a86544e39d628f5b0c47f9442ee2
JamilaFatima34 <jamilafatima7896@gmail.com>
2025-03-07 11:14:52 +0500

    Added Addition Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,2 +1,12 @@
 Console.WriteLine("Jamila Fatima");
 Console.WriteLine("24-St-042");
+
+Code for Addition:
+int num1 = 10;
+int num2 = 14;
+int sum = num1 + num2;
+Console.WriteLine("The sum of " + num1 + "and " + num2 + "is: " + sum);
+  
+
+
+
