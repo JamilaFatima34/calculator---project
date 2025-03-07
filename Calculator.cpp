2d3aad58d15c532b7d1bf034151f680eb9b15f62
JamilaFatima34 <jamilafatima7896@gmail.com>
2025-03-07 11:27:16 +0500

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,2 +1,8 @@
 Console.WriteLine("Jamila Fatima");
 Console.WriteLine("24-St-042");
+Code for Multiplication:
+
+int num1 = 23;
+int num2 = 13;
+int product = num1 * num2;
+Console.WriteLine("The PRODUCT between " + num1 + "and " + num2 + " is: "+ product);
