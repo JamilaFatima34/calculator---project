2e60f8dd2b2ad734a8d97fee64e82bfdb715b90d
JamilaFatima34 <jamilafatima7896@gmail.com>
2025-03-07 11:32:45 +0500

    Added Division Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,2 +1,8 @@
 Console.WriteLine("Jamila Fatima");
 Console.WriteLine("24-St-042");
+Code for Division:
+
+int num1 = 23;
+int num2 = 13;
+int division = num1 / num2;
+Console.WriteLine("The division between " + num1 + "and " + num2 + " is: "+ division);
